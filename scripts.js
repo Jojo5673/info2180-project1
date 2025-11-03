3882d3a970c79e31b92fde6ead8e1d4d4f79d368
Jojo5673 <judoudo2005@gmail.com>
2025-11-03 12:43:39 -0500

    Added javascript to email box

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function() {
+    const form  = document.getElementsByTagName("form")[0];
+    const messageBox = document.getElementsByClassName("message")[0];
+    
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let email = event.target[0].value;
+        messageBox.textContent = email !== ""?`Thank you! Your email address ${email} has been added to our mailing list!`:"Please enter a valid email address";
+    })
+
+
+});
